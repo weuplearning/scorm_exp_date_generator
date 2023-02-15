ce7ed6aeae35e3ca848547771d8dd6f640e302ce
Annojan <annojan.kandiah@weuplearning.com>
2023-02-15 11:34:03 +0100

    added error handling and showing the errors on the gui -added check if the fields are empty -added check if the path is correct -added check if the lms/scormfile.js is present -added check if the expDateFunction.txt file is present
    now the file browser is limited to show only folders and zip files

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
-
-
-# TODO  
+# TODO
 # rezipper avec les dossier et sous dosseir OK
 # suppprimer dosseir OK
 # mettre la fonction js avec les variables, propre avec retour à la ligne OK
@@ -15,7 +13,6 @@ import os
 import sys
 import zipfile
 
-
 from PyQt5.QtWidgets import *
 from PyQt5 import QtWidgets
 from PyQt5.QtGui import *
@@ -27,8 +24,8 @@ class MainWindow(QMainWindow):
     def __init__(self):
         QMainWindow.__init__(self)
 
-        self.setMinimumSize(QSize(420, 300))    
-        self.setWindowTitle("Scorm Date Generator") 
+        self.setMinimumSize(QSize(420, 300))
+        self.setWindowTitle("Scorm Date Generator")
 
         options = QFileDialog.Options()
         options |= QFileDialog.DontUseNativeDialog
@@ -38,15 +35,14 @@ class MainWindow(QMainWindow):
         self.nameLabel.move(20, 80)
 
         self.date = QDateEdit(self)
-        # setting geometry of the date edit 
+        # setting geometry of the date edit
         self.date.setGeometry(100, 80, 200, 30)
 
         # date
         d = QDate(2020, 6, 10)
- 
+
         # setting date to the date edit
         self.date.setDate(d)
-
 
         self.scormLabel = QLabel(self)
         self.scormLabel.setText('Scorm file :')
@@ -55,7 +51,6 @@ class MainWindow(QMainWindow):
         self.scorm_message.move(100, 20)
         self.scorm_message.resize(200, 32)
         self.scormLabel.move(20, 20)
-
 
         self.nameLabel = QLabel(self)
         self.nameLabel.setText('Message :')
@@ -69,31 +64,44 @@ class MainWindow(QMainWindow):
         self.line_message.resize(200, 32)
         self.nameLabel.move(20, 140)
 
+        self.error_message = QMessageBox()
+        self.error_message.setIcon(QMessageBox.Critical)
+        self.error_message.setText("Veuillez remplir tous les champs")
+        self.error_message.setWindowTitle("Informations manquantes")
+
+        self.error_dialog = QtWidgets.QErrorMessage()
+
         pybutton = QPushButton('OK', self)
         pybutton.clicked.connect(self.click_method)
-        pybutton.resize(200,32)
-        pybutton.move(100, 200)        
+        pybutton.resize(200, 32)
+        pybutton.move(100, 200)
 
         pybutton = QPushButton('browse', self)
         pybutton.clicked.connect(self.browse_scorm)
-        pybutton.resize(60,32)
-        pybutton.move(320, 20)        
+        pybutton.resize(60, 32)
+        pybutton.move(320, 20)
+
+    def show_error_message(self, error):
+        error_message = QMessageBox()
+        error_message.setIcon(QMessageBox.Critical)
+        error_message.setText(error)
+        error_message.setWindowTitle("Erreur")
+        error_message.exec_()
 
     def browse_scorm(self):
         path = QFileDialog.getOpenFileName(self, 'Open a file', '',
-                                        'All Files (*.*)')
+                                           'Zip Files (*.zip)')
         if path != ('', ''):
             print(path[0])
 
             self.scorm_message.setText(path[0])
             print(self.scorm_message.text())
-            
-
 
     def click_method(self):
-        self.update_scorm_date(self.date.date().toPyDate(), self.line_message.text(),self.scorm_message.text())
-
-
+        if not self.scorm_message.text() or not self.line_message.text() or not self.date.text():
+            self.error_message.exec_()
+        else:
+            self.update_scorm_date(self.date.date().toPyDate(), self.line_message.text(), self.scorm_message.text())
 
     def update_scorm_date(self, new_date, new_message, scorm_name):
 
@@ -138,40 +146,39 @@ class MainWindow(QMainWindow):
         folder_name = scorm_name.replace('.zip', '')
         file_name = scorm_name.rsplit('/', 1)[-1]
         path = scorm_name.replace(file_name, '')
-        print("folder_name: " + folder_name)
-        print("file_name: " + file_name)
-        print("path: " + path)
 
-        if not os.path.exists(folder_name):
-            os.makedirs(folder_name)
+        # extract firectory from zip file
+        try:
+            with zipfile.ZipFile(scorm_name, 'r') as zipObject:
+                zipObject.extractall(folder_name)
+        except OSError as error:
+            self.show_error_message(str(error))
+            return
 
-        #extract firectory from zip file
-        with zipfile.ZipFile(scorm_name, 'r') as zipObject:
-            zipObject.extractall(folder_name)
+        # replace date with input values
+        try:
+            with open('expDateFunction.txt') as dataFile:
+                data = dataFile.read()
+                js_data = data.replace('SCORM_EXP_MSG', new_message).replace('SCORM_EXP_DATE',
+                                                                             str(scorm_expiration_date))
+        except OSError as error:
+            self.show_error_message(str(error))
+            return
 
-        #replace date with input values
-        with open('expDateFunction.txt') as dataFile:
-            data = dataFile.read()
-            js_data = data.replace('SCORM_EXP_MSG', new_message).replace('SCORM_EXP_DATE', str(scorm_expiration_date))
+        try:
+            with open(folder_name + '/lms/scormdriver.js', 'a', encoding='utf-8') as f:
+                f.write("\n")
+                f.write(js_data)
+        except OSError as error:
+            self.show_error_message(str(error))
+            return
 
-        with open(folder_name + '/lms/scormdriver.js','a',encoding = 'utf-8') as f:
-            f.write("\n")
-            f.write(js_data)
-
-        #replace the old zip files with the updated one
-        # zf = zipfile.ZipFile(path + file_name_date + file_name, "w")
-        # for dirname, subdirs, files in os.walk(folder_name):
-        #     updated_dirname = dirname.replace(path,'')
-        #     zf.write(updated_dirname)
-        #     for filename in files:
-        #         zf.write(os.path.join(updated_dirname, filename))
-        # zf.close()
-
+        # replace the old zip files with the updated one
         shutil.make_archive(path + file_name_date + file_name, format='zip', root_dir=folder_name)
 
         shutil.rmtree(folder_name, ignore_errors=False, onerror=None)
 
-        #FOR DEV
+        # FOR DEV
         self.close()
 
         print("**************************** end of process ***************************")
@@ -181,4 +188,4 @@ if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     mainWin = MainWindow()
     mainWin.show()
-    sys.exit( app.exec_() )
+    sys.exit(app.exec_())
